a12701105a5d1c06962c8c1acd36af54247a595f
AryanRogye <aryan.rogye@gmail.com>
2024-05-24 16:01:11 -0500

    made a generateRandom because I realized that I have to generate random everytime I move so its usefull later on

--- main.cpp
+++ main.cpp
@@ -18,42 +18,51 @@ public:
     void handleLeft();
     void handleRight();
 
+    void generateRandom();
+
     void startGame();
     void displayBoard();
 };
 
 Board::Board() {
     this->SIZE = 4;
-    /**
-     * since random spawning is 2x need to get both rand locations
-     */
-    int randRow1 = rand() % 4;
-    int randCol1 = rand() % 4;
-
-    int randRow2 = rand() % 4;
-    int randCol2 = rand() % 4;
-    // if the 2nd locations is equal to the first then need to change again
-    while (randRow2 == randRow1 && randCol1 == randCol2) {
-        randRow2 = rand() % 4;
-        randCol2 = rand() % 4;
-    }
-
-    cout << "rand row -- " << randRow1 << " rand col -- " << randCol1 << endl;
-    cout << "rand row -- " << randRow2 << " rand col -- " << randCol2 << endl;
 
     for (int row = 0; row < this->SIZE; row++) {
         for (int col = 0; col < this->SIZE; col++) {
-            if (row == randRow1 && col == randCol1) {
-                this->board[row][col] = 2;
-                continue;
-            }
-            if (row == randRow2 && col == randCol2) {
-                this->board[row][col] = 2;
-                continue;
-            }
             this->board[row][col] = 0;
         }
     }
+    generateRandom();
+    generateRandom();
+}
+
+void Board::generateRandom() {
+
+    int emptyCells = 0;
+
+    //checks how many empty cells there are
+    for (int row = 0; row < SIZE; row++) {
+        for (int col = 0; col < SIZE; col++) {
+            if (this->board[row][col] == 0) {
+                emptyCells++;
+            }
+        }
+    }
+
+    //if none then u lost
+    if (emptyCells == 0) {
+        return;
+    }
+
+    int randRow = rand() % 4;
+    int randCol = rand() % 4;
+
+    while (this -> board[randRow][randCol] != 0) {
+        randRow = rand() % 4;
+        randCol = rand() % 4;
+    }
+
+    this -> board[randRow][randCol] = 2;
 }
 
 void Board::displayBoard() {
