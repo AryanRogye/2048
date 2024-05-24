ffea86d2b4cfd7b38958b2b387fa464b7c7e117d
AryanRogye <aryan.rogye@gmail.com>
2024-05-24 14:51:15 -0500

    working on feature 1

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 
 using namespace std;
 
+class main {
+    public:
+        main() {
+            cout << "constructor" << endl;
+        }
+};
+
 int main() {
     cout << "Hello World!" << endl;
     cout << "Goodbye World!!!" << endl;
