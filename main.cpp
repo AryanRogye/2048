c6a3b5a7301a2d60f815bf66566e7d38b3bcf07f
AryanRogye <aryan.rogye@gmail.com>
2024-05-24 15:01:00 -0500

    Made Board class with constructor

--- main.cpp
+++ main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+class Board {
+
+private:
+    int board[4][4];
+
+public:
+
+    Board() {
+        cout << "Constructor Made" << endl;
+    }
+
+};
+
 int main() {
-    
+    Board b;
 }
