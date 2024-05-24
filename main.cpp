92deefe66cd4dbc9b9076a97227d24cae24d4ca4
AryanRogye <aryan.rogye@gmail.com>
2024-05-24 15:06:17 -0500

    board is made and size is set

--- main.cpp
+++ main.cpp
@@ -6,15 +6,31 @@ class Board {
 
 private:
     int board[4][4];
-
+    int SIZE;
 public:
 
     Board() {
-        cout << "Constructor Made" << endl;
+        this -> SIZE = 4;
+
+        for(int row = 0; row < this -> SIZE; row++) {
+            for(int col = 0; col < this -> SIZE; col++) {
+                this -> board[row][col] = 0;
+            }
+        }
+    }
+
+    void displayBoard() {
+        for(int row = 0; row < this -> SIZE; row++) {
+            for(int col = 0; col < this -> SIZE; col++) {
+                cout << this -> board[row][col] << " ";
+            }
+            cout << endl;
+        }
     }
 
 };
 
 int main() {
     Board b;
+    b.displayBoard();
 }
