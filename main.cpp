c6924b1125ba72072a50f8aad6c74c1a8ded415e
AryanRogye <aryan.rogye@gmail.com>
2024-05-24 14:58:21 -0500

    Starting 2048 project

--- main.cpp
+++ main.cpp
@@ -2,16 +2,6 @@
 
 using namespace std;
 
-class main {
-    public:
-        main() {
-            cout << "constructor" << endl;
-        }
-};
-
 int main() {
-    cout << "Hello World!" << endl;
-    cout << "Goodbye World!!!" << endl;
-    cout << "test" << endl;
-    cout << "test" << endl;
+    
 }
