d88b728a35871d4897c0c8d0863c68dffab88243
AryanRogye <aryan.rogye@gmail.com>
2024-05-24 15:50:33 -0500

    cleaned up code and added outside the class

--- main.cpp
+++ main.cpp
@@ -11,71 +11,100 @@ private:
     int SIZE;
 public:
 
-    Board() {
-        this -> SIZE = 4;
+    Board();
 
-        /**
-         * since random spawning is 2x need to get both rand locations
-        */
-        int randRow1 = rand() % 4;
-        int randCol1 = rand() % 4;
+    void handleUp();
+    void handleDown();
+    void handleLeft();
+    void handleRight();
 
-        int randRow2 = rand() % 4;
-        int randCol2 = rand() % 4;
-        //if the 2nd locations is equal to the first then need to change again
-        while(randRow2 == randRow1 && randCol1 == randCol2) {
-            randRow2 = rand() % 4;
-            randCol2 = rand() % 4;
-        }
-
-        cout << "rand row -- " << randRow1 << " rand col -- " << randCol1 << endl;
-        cout << "rand row -- " << randRow2 << " rand col -- " << randCol2 << endl;
-
-        for(int row = 0; row < this -> SIZE; row++) {
-            for(int col = 0; col < this -> SIZE; col++) {
-                if(row == randRow1 && col == randCol1) {
-                    this -> board[row][col] = 2;
-                    continue;
-                }
-                if(row == randRow2 && col == randCol2) {
-                    this -> board[row][col] = 2;
-                    continue;
-                }
-                this -> board[row][col] = 0;
-            }
-        }
-    }
-
-    void displayBoard() {
-        for(int row = 0; row < this -> SIZE; row++) {
-            for(int col = 0; col < this -> SIZE; col++) {
-                cout << this -> board[row][col] << " ";
-            }
-            cout << endl;
-        }
-    }
-
-    void startGame() {
-        string input = "";
-        while(input != "q") {
-            displayBoard();
-            cout << "Enter Your Next Move : ";
-            cin >> input;
-            if(input == "\033[A") {
-                cout << "up" << endl;
-            }
-            if(input == "\033[D") {
-                cout << "left" << endl;
-            }
-            if(input == "\033[B") {
-                cout << "down" << endl;
-            }
-            if(input == "\033[C") {
-                cout << "right" << endl;
-            }
-        }
-    }
+    void startGame();
+    void displayBoard();
 };
+
+Board::Board() {
+    this->SIZE = 4;
+    /**
+     * since random spawning is 2x need to get both rand locations
+     */
+    int randRow1 = rand() % 4;
+    int randCol1 = rand() % 4;
+
+    int randRow2 = rand() % 4;
+    int randCol2 = rand() % 4;
+    // if the 2nd locations is equal to the first then need to change again
+    while (randRow2 == randRow1 && randCol1 == randCol2) {
+        randRow2 = rand() % 4;
+        randCol2 = rand() % 4;
+    }
+
+    cout << "rand row -- " << randRow1 << " rand col -- " << randCol1 << endl;
+    cout << "rand row -- " << randRow2 << " rand col -- " << randCol2 << endl;
+
+    for (int row = 0; row < this->SIZE; row++) {
+        for (int col = 0; col < this->SIZE; col++) {
+            if (row == randRow1 && col == randCol1) {
+                this->board[row][col] = 2;
+                continue;
+            }
+            if (row == randRow2 && col == randCol2) {
+                this->board[row][col] = 2;
+                continue;
+            }
+            this->board[row][col] = 0;
+        }
+    }
+}
+
+void Board::displayBoard() {
+    for(int row = 0; row < this -> SIZE; row++) {
+        for(int col = 0; col < this -> SIZE; col++) {
+            cout << this -> board[row][col] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void Board::startGame() {
+    string input = "";
+    while(input != "q") {
+        displayBoard();
+        cout << "Enter Your Next Move : ";
+        cin >> input;
+        if(input == "\033[A") {
+            cout << "up" << endl;
+            handleUp();
+        }
+        if(input == "\033[D") {
+          cout << "left" << endl;
+            handleLeft();
+        }
+        if(input == "\033[B") {
+            cout << "down" << endl;
+            handleDown();
+        }
+        if(input == "\033[C") {
+            cout << "right" << endl;
+            handleRight();
+        }
+    }
+}
+
+void Board::handleUp() {
+
+}
+
+void Board::handleDown() {
+
+}
+
+void Board::handleLeft() {
+
+}
+
+void Board::handleRight() {
+
+}
 
 int main() {
     srand(time(nullptr));
